7fd0d7f235e6566dc9616c3a256da57f503c7f30
kulkaktus <davidhw@stud.ntnu.no>
2017-04-27 12:37:46 +0200

    almost finito ex 2

--- Helicopter lab part II, Optimal boogaloo/puttipluttiplott/problem2.m
+++ Helicopter lab part II, Optimal boogaloo/puttipluttiplott/problem2.m
@@ -1,8 +1,8 @@
 
 figure(1)
-plot(oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:),...
-    oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:));
-title('Q = 0.1, optimal and measured states');
+plot(oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:),...
+    oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:));
+title('Q = 1, optimal and measured states');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
 legend('Optimal', 'Measured');

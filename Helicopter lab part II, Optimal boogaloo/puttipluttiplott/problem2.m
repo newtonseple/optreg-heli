adfea90dad85513be39e82c0ec9caba00385eb61
kulkaktus <davidhw@stud.ntnu.no>
2017-04-28 12:46:43 +0200

    Fixed plots, Q->q

--- Helicopter lab part II, Optimal boogaloo/puttipluttiplott/problem2.m
+++ Helicopter lab part II, Optimal boogaloo/puttipluttiplott/problem2.m
@@ -2,7 +2,7 @@
 figure(1)
 plot(oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:),...
     oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:));
-title('Q = 1, optimal and measured states');
+title('q = 1, optimal and measured states');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
 legend('Optimal', 'Measured');
@@ -11,25 +11,25 @@ figure(2)
 plot(oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:),...
     oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:),...
     oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:),oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(7,:));
-title('Q = 0.1, Q = 1 and Q = 10,  measured states');
+title('q=0.1, q=1 and q=10, measured \lambda');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
-legend('Q=0.1', 'Q=1', 'Q=10');
+legend('q=0.1', 'q=1', 'q=10');
 
 figure(3)
 plot(oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:),...
     oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:),...
     oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:),oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(2,:));
-title('Q = 0.1, Q = 1 and Q = 10,  optimal states');
+title('q=0.1, q=1 and q=10, optimal states');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
-legend('Q=0.1', 'Q=1', 'Q=10');
+legend('q=0.1', 'q=1', 'q=10');
 
 figure(4)
 plot(oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q01_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(6,:),...
     oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:), oppg2Q1_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(6,:),...
     oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(1,:),oppg2Q10_x1o_x2o_x3o_x4o_u1o_x1_x2_x3_x4_u1_out(6,:));
-title('Q = 0.1, Q = 1 and Q = 10,  Different optimal inputs');
+title('q=0.1, q=1 and q=10, different optimal inputs');
 xlabel('Time [s]');
 ylabel('Angle [rad]');
-legend('Q=0.1', 'Q=1', 'Q=10');
+legend('q=0.1', 'q=1', 'q=10');
